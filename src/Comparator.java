fbefd78af762aa431310b0f1475fab260f3bc55a
SerMURZIK <118277030+SerMURZIK@users.noreply.github.com>
2023-01-06 11:42:03 +0400

    Added compare by amount of words of surnames

--- src/Comparator.java
+++ src/Comparator.java
@@ -7,7 +7,15 @@ public class Comparator implements java.util.Comparator<Person> {
 
     @Override
     public int compare(Person o1, Person o2) {
-        if (getAmountOfWordsOfSurname(o1.getSurname()) >= maxAmountOfWords && getAmountOfWordsOfSurname(o2.getSurname()) >= maxAmountOfWords) {
+        if (getAmountOfWordsOfSurname(o1.getSurname()) == getAmountOfWordsOfSurname(o2.getSurname()))  {
+            return Integer.compare(o1.getAge(), o2.getAge());
+        }
+
+        if (getAmountOfWordsOfSurname(o1.getSurname()) != getAmountOfWordsOfSurname(o2.getSurname())) {
+            return Integer.compare(getAmountOfWordsOfSurname(o1.getSurname()), getAmountOfWordsOfSurname(o2.getSurname()));
+        }
+
+        if (getAmountOfLettersOfSurname(o1.getSurname()) >= maxAmountOfWords && getAmountOfLettersOfSurname(o2.getSurname()) >= maxAmountOfWords) {
             return Integer.compare(o1.getAge(), o2.getAge());
         } else {
             if (o1.getSurname().compareTo(o2.getSurname()) != 0) {
@@ -19,6 +27,10 @@ public class Comparator implements java.util.Comparator<Person> {
     }
 
     private int getAmountOfWordsOfSurname(String surname) {
+        return surname.split("-").length;
+    }
+
+    private int getAmountOfLettersOfSurname(String surname) {
         return surname.split("").length;
     }
 }
